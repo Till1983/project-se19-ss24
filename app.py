4a51490317a0ca4df086e243284a9e5d24252f25
Till1983 <till_ermold@web.de>
2024-03-10 20:17:32 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/posts')
+def posts():
+    return render_template("posts.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
