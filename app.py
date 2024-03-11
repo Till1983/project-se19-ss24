4e986a7ac073dd460cb363e6a411e2ba3a2bafe7
Till1983 <till_ermold@web.de>
2024-03-11 23:53:37 +0100

    add comment to articles dictionary

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+# This is the basic structure of how the post are going to appear later in the database. The structure will of course be modified.
 articles = {
     1: { 'title': 'The First Post', 'content': 'This is the first post.' },
     2: { 'title': 'The Second Post', 'content': 'This is the second post.' },
