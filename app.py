62aff8ff636b2ab59bc3011e52ffb68ba7e17bea
Till1983 <till_ermold@web.de>
2024-03-12 00:12:47 +0100

    moved articles dictionary to helper file

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
+from helper import articles
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
-
-# This is the basic structure of how the post are going to appear later in the database. The structure will of course be modified.
-articles = {
-    1: { 'title': 'The First Post', 'content': 'This is the first post.' },
-    2: { 'title': 'The Second Post', 'content': 'This is the second post.' },
-    3: { 'title': 'The Third Post', 'content': 'This is the third post.' }
-}
 
 @app.route('/')
 @app.route('/home')
---
+++ helper.py
@@ -0,0 +1,6 @@
+# This is the basic structure of how the post are going to appear later in the database. The structure will of course be modified.
+articles = {
+    1: { 'title': 'The First Post', 'content': 'This is the first post.' },
+    2: { 'title': 'The Second Post', 'content': 'This is the second post.' },
+    3: { 'title': 'The Third Post', 'content': 'This is the third post.' }
+}
