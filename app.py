9c3316a4aef360a1f429ac605dd0494ff6289c8c
Till1983 <till_ermold@web.de>
2024-03-11 20:31:22 +0100

    add dictionary articles and route to posts

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+articles = {
+    1: { 'title': 'The First Post', 'content': 'This is the first post.' },
+    2: { 'title': 'The Second Post', 'content': 'This is the second post.' },
+    3: { 'title': 'The Third Post', 'content': 'This is the third post.' }
+}
+
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
@@ -19,6 +25,14 @@ def contact():
 @app.route('/posts')
 def posts():
     return render_template("posts.html")
+
+@app.route('/posts/<int:post_id>')
+def post(post_id):
+    post = articles.get(post_id)
+    if post:
+        return render_template("post.html", post=post)
+    else:
+        return render_template("error.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
